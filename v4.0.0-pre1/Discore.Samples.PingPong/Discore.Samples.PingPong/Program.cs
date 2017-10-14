1adf2a54cf1cd3f680271f77c596fec26c680aba
Unknown <elafrenais12@gmail.com>
2017-10-14 17:28:53 -0400

    Add v4.0.0-pre1/PingPong

---
+++ v4.0.0-pre1/Discore.Samples.PingPong/Discore.Samples.PingPong/Program.cs
@@ -0,0 +1,61 @@
+﻿using Discore;
+using Discore.Http;
+using Discore.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Discore.Samples.PingPong
+{
+    class Program
+    {
+        DiscordHttpClient http;
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run().Wait();
+        }
+
+        public async Task Run()
+        {
+            const string TOKEN = "<bot user token goes here>";
+
+            // Create an HTTP client.
+            http = new DiscordHttpClient(TOKEN);
+
+            // Create a single shard.
+            using (Shard shard = new Shard(TOKEN, 0, 1))
+            {
+                // Subscribe to the message creation event.
+                shard.Gateway.OnMessageCreated += Gateway_OnMessageCreated;
+
+                // Start the shard.
+                await shard.StartAsync();
+                Console.WriteLine("Bot started!");
+
+                // Wait for the shard to end before closing the program.
+                await shard.WaitUntilStoppedAsync();
+            }
+        }
+
+        private async void Gateway_OnMessageCreated(object sender, MessageEventArgs e)
+        {
+            Shard shard = e.Shard;
+            DiscordMessage message = e.Message;
+
+            if (message.Author.Id == shard.UserId)
+                // Ignore messages created by our bot.
+                return;
+
+            if (message.Content == "!ping")
+            {
+                try
+                {
+                    // Reply to the user who posted "!ping".
+                    await http.CreateMessage(message.ChannelId, $"<@!{message.Author.Id}> Pong!");
+                }
+                catch (DiscordHttpApiException) { /* Message failed to send... :( */ }
+            }
+        }
+    }
+}
---
+++ v4.0.0-pre1/Discore.Samples.PingPong/README.md
@@ -0,0 +1,11 @@
+# Discore.Samples.PingPong
+The simplest example of them all! 
+
+## Setup
+Simply replace the `TOKEN` constant on line 21 with your bot's token.
+
+## Usage
+Enter the message "!ping" in any channel the bot has `READ_MESSAGES` and `SEND_MESSAGES` permissions.
+
+## Discore Version
+This sample uses Discore 4.0.0-pre1.
